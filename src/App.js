362776508848a8b5b2c6def60d4f8366fb96175d
SirTLD <ekanemita@gmail.com>
2021-09-18 00:44:32 -0500

    update to layout and logic

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <Title />
-      {/* <Game /> */}
       <Grid />
-      <button>Start New Ga</button>
     </div>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,39 +1,105 @@
 import React, { useState } from 'react';
-import Square from './Square';
 
 const Grid = () => {
-  const EMPTY = 'EMPTY';
-  const CIRCLE = 'CIRCLE';
-  const CROSS = 'CROSS';
+  const [turn, setTurn] = useState('X');
+  const [cells, setCells] = useState(Array(9).fill(''));
 
-  const [state, setState] = useState({
-    player: CROSS,
-    positions: [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  });
+  const [winner, setWinner] = useState();
 
-  const makeTurn = (position) => {
-    const positions = [...state.positions];
-    positions[position] = state.player;
+  const winnerCheck = (squares) => {
+    let winningCombo = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagnal: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
 
-    setState({
-      player: state.player === CIRCLE ? CROSS : CIRCLE,
-    });
+    for (let combo in winningCombo) {
+      winningCombo[combo].forEach((winpattern) => {
+        if (
+          squares[winpattern[0]] === '' ||
+          squares[winpattern[1]] === '' ||
+          squares[winpattern[2]] === ''
+        ) {
+        } else if (
+          squares[winpattern[0]] === squares[winpattern[1]] &&
+          squares[winpattern[1]] === squares[winpattern[2]]
+        ) {
+          setWinner(squares[winpattern[0]]);
+        }
+      });
+    }
   };
 
+  const handleClick = (num) => {
+    if (cells[num] !== '') {
+      alert('Choose another square');
+      return;
+    }
+    let squares = [...cells];
+
+    if (turn === 'X') {
+      squares[num] = 'X';
+      setTurn('O');
+    } else {
+      squares[num] = 'O';
+      setTurn('X');
+    }
+
+    winnerCheck(squares);
+    setCells(squares);
+  };
+
+  const restartGame = () => {
+    setWinner(null);
+    setCells(Array(9).fill(''));
+  };
+
+  function Square({ num }) {
+    return (
+      <>
+        <div className="square_unit" onClick={() => handleClick(num)}>
+          {cells[num]}
+        </div>
+      </>
+    );
+  }
+
   return (
-    <div className="grid_container">
+    <>
+      {!winner && <h1 className="game-title">Turn: Player {turn}</h1>}
+      {winner && <h1 className="game-title">Game Over!</h1>}
+
       <div className="grid">
-        <Square position={0} value={state.positions[0]} makeTurn={makeTurn} />
-        <Square position={1} value={state.positions[1]} makeTurn={makeTurn} />
-        <Square position={2} value={state.positions[2]} makeTurn={makeTurn} />
-        <Square position={3} value={state.positions[3]} makeTurn={makeTurn} />
-        <Square position={4} value={state.positions[4]} makeTurn={makeTurn} />
-        <Square position={5} value={state.positions[5]} makeTurn={makeTurn} />
-        <Square position={6} value={state.positions[6]} makeTurn={makeTurn} />
-        <Square position={7} value={state.positions[7]} makeTurn={makeTurn} />
-        <Square position={8} value={state.positions[8]} makeTurn={makeTurn} />
+        <Square num={0} click={handleClick} />
+        <Square num={1} click={handleClick} />
+        <Square num={2} click={handleClick} />
+        <Square num={3} click={handleClick} />
+        <Square num={4} click={handleClick} />
+        <Square num={5} click={handleClick} />
+        <Square num={6} click={handleClick} />
+        <Square num={7} click={handleClick} />
+        <Square num={8} click={handleClick} />
       </div>
-    </div>
+
+      {winner && (
+        <>
+          <h1 className="game-tag">Player {winner} has won! </h1>
+
+          <button onClick={restartGame}>Start New Game</button>
+        </>
+      )}
+    </>
   );
 };
 
